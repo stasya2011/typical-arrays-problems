504d6f699bd1bd07157cba1dfb375b9cc1677644
Stasya Geleysha <s.geleisha@gmail.com>
2020-10-02 22:04:42 +0300

    init: start to do arrays

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 
 exports.min = function min (array) {
+
+  if (array.length == 0) {
+    return 0
+} else {
+    var onlyNumberArr = array.filter((item) => typeof item === "number");
+    // console.log(onlyNumberArr);
+    for (let i = 0; i < onlyNumberArr.length; i++) {
+        if (onlyNumberArr[i] != 0) {
+            return Math.min(...onlyNumberArr);
+
+        }
+    }
+
+}
+  
   return 0;
 }
 
@@ -10,3 +25,22 @@ exports.max = function max (array) {
 exports.avg = function avg (array) {
   return 0;
 }
+
+
+
+// function max(...theArgs) {
+//   if (theArgs == 0 || theArgs == null) {
+//       return 0
+//   } else {
+//       var onlyNumberArr = theArgs.filter((item) => typeof item === "number");
+//       // console.log(onlyNumberArr);
+//       for (let i = 0; i < onlyNumberArr.length; i++) {
+//           if (onlyNumberArr[i] != 0) {
+//               return Math.max(...onlyNumberArr);
+
+//           }
+//       }
+
+//   }
+// }
+
